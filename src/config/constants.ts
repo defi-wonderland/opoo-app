461b7edc47c2910360d6a625c493133b95b0209e
Ardy <116569704+0xArdy@users.noreply.github.com>
2023-08-30 17:07:45 -0400

    build: add workflow to deploy on vercel (#53)

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,6 +3,7 @@ import { Constants } from '~/types';
 const constants: Constants = {
   ORACLE: '0xC7F52019DfE600993c8088383668eFf1FBa4473a',
 };
+
 export const getConstants = (): Constants => {
   return constants;
 };
